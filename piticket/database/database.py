96cb3a44ae596b4e6992405b3b8d2fd2557d696e
Tertese Moses <smatnemo25@gmail.com>
2024-06-29 22:22:44 +0100

    Created Database object

---
+++ piticket/database/database.py
@@ -0,0 +1,40 @@
+"""
+
+"""
+from sqlite3 import dbapi2 as Database
+
+class DB:
+    def __init__(self, filename):
+        """
+        :attr filename: the path of the database
+        :type filename: str
+        :attr _conn: connection to the database
+        :type _conn: Connection
+        :attr _cursor: cursor for the database
+        :type _cursor: Cursor
+        """
+        self.filename = filename
+        self.is_open = False
+        self._conn = self.open()
+        self._cursor = self._conn.cursor()
+        
+    
+    def open(self):
+        """Open database and return a connection to the database.
+        """
+        if not self.is_open:
+            conn = Database.Connection(self.filename)
+            self.is_open = True
+            return conn 
+        self.is_open = False
+
+    def close(self):
+        """Close the connection to the database.
+        """
+        if self.is_open:
+            self._cursor.commit()
+            self._conn.close()
+            self.is_open = False
+
+
+
