9db11ad09e7f45b5b8f182e0cfe439f4d105fe83
Tertese Moses <smatnemo25@gmail.com>
2024-06-29 22:33:08 +0100

    A model of a table

---
+++ piticket/database/model.py
@@ -0,0 +1,24 @@
+
+
+class Model:
+    def __init__(self, db):
+        """
+        :attr _db: dabatabase object to represent a session
+        :type _db: piticket.database.DB
+        """
+        self._db = db 
+
+    def insert(self):
+        pass 
+    
+    def update(self):
+        pass
+    
+    def insert(self):
+        pass 
+    
+    def delete(self):
+        pass 
+
+    def create(self):
+        pass
