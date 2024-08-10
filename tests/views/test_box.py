165d6724e1cdd2dceae7d56c34671805852d98db
Tertese Samuel Moses <smatnemo25@gmail.com>
2024-08-10 17:20:51 +0100

    Completed test for box draw method

---
+++ tests/views/test_box.py
@@ -0,0 +1,23 @@
+import pygame
+from piticket.views.box import Box
+
+box = Box(x=10, y=10, 
+        width=100, height=60, 
+        margin=20, padding=10, 
+        border=1, border_radius=3, 
+        border_color=(0,0,0), 
+        content='Button', 
+        content_color=(0,0,0), 
+        color=(255, 255, 255), 
+        interactable=True)
+
+# Initialize pygame and set display
+pygame.init()
+win = pygame.display.set_mode((800,400), pygame.RESIZABLE)
+win.fill((255,0,0))
+
+
+def test_draw(view_loop):
+    view_loop(box.draw, win)
+
+        
