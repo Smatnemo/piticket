f42650531728259e95e56ba4d9df62a2ffdb0591
Tertese Samuel Moses <smatnemo25@gmail.com>
2024-08-10 17:22:05 +0100

    Created tests for video and intro background

---
+++ tests/views/test_window.py
@@ -0,0 +1,13 @@
+import pygame
+from piticket.views.window import PiWindow
+
+win = PiWindow('Ticket App Test')
+
+
+def test_init(view_loop):
+    view_loop(win.show_intro)
+
+def test_video(view_loop):
+    view_loop(win.show_video)
+
+
