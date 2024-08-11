554ea5f3aba1b847d9f1a92f435b26040e03ff64
Tertese Samuel Moses <smatnemo25@gmail.com>
2024-08-11 17:29:56 +0100

    new size by croping method and test

--- piticket/pictures/sizing.py
+++ piticket/pictures/sizing.py
@@ -33,3 +33,77 @@ def new_size_keep_aspect_ratio(original_size:tuple, target_size:tuple, resize_ty
             ty = scale_factor * oy
     return (int(tx),int(ty))
 
+def new_size_by_croping_ratio(original_size, target_size, crop_type='center'):
+    """Return a tuple of the top-left and bottom-right points (x1, y1, x2, y2) by
+    croping the original size but corresponding to the aspect ratio of the target size
+    
+    Note: target_size is only used to calculate aspect ratio, the returned size doesn't fit
+    to it.
+    
+    The position of the rectangle can be determined by the crop_type
+    
+    * top-left
+    * top-center
+    * top-right
+    * center-left
+    * center
+    * center-right
+    * bottom-left
+    * bottom-center
+    * bottom-right
+    """
+
+    # Get current and desired ratio of the images
+    img_ratio = original_size[0] / float(original_size[1])
+    target_ratio = target_size[0] / float(target_size[1])
+
+    tx, ty = original_size 
+    if target_ratio > img_ratio:
+        # crop on constant width
+        ty = int(original_size[0] / target_ratio)
+    elif target_ratio < img_ratio:
+        # crop on constant height
+        tx = int(target_ratio * original_size[1])
+    
+    x, y = position_picture(original_size, (tx,ty), crop_type)
+
+    return (x, y, tx + x, ty + y)
+
+def new_size_by_croping(original_size, target_size, crop_type='center'):
+    """Return a tuple of top-left and bottom-right points (x1, y1, x2, y2) corresponding
+    to a crop of the original size. The position of the rectangle can be defined by the
+    crop_type parameter.
+    crop_type parameter:
+    
+        * top-left
+        * top-center
+        * top-right
+        * center-left
+        * center
+        * center-right
+        * bottom-left
+        * bottom-center
+        * bottom-right
+    """    
+    x, y = position_picture(original_size, target_size, crop_type)
+    return x, y, target_size[0] + x, target_size[1] + y
+
+def position_picture(original_size, target_size, crop_type):
+    """Return a tuple of x, y position
+    """
+    x, y = 0, 0
+    if crop_type.endswith('left'):
+        x = 0
+    elif crop_type.endswith('center'):
+        x = (original_size[0] - target_size[0]) // 2
+    elif crop_type.endswith('right'):
+        x = original_size[0] - target_size[0]
+
+    if crop_type.startswith('top'):
+        y = 0
+    elif crop_type.startswith('center'):
+        y = (original_size[1] - target_size[1]) // 2
+    elif crop_type.startswith('bottom'):
+        y = original_size[1] - ty 
+
+    return x, y
--- tests/test_pictures.py
+++ tests/test_pictures.py
@@ -1,17 +1,24 @@
 import os.path as osp
 from PIL import Image
 from piticket.pictures import get_filename, get_pygame_image
-from piticket.pictures.sizing import new_size_keep_aspect_ratio
+from piticket.pictures.sizing import (new_size_keep_aspect_ratio, 
+                                    new_size_by_croping_ratio, new_size_by_croping)
 
-name = 'camera.png'
+name = 'pibooth.png'
 full_name = osp.join('/home/pi/Dev/piticket/piticket/pictures/assets',name)
+image = Image.open(full_name)
 
 def test_get_filename():
     assert full_name == get_filename(name)
 
 def test_new_size_keep_aspect_ratio():
-    image = Image.open(full_name)
     new_size_keep_aspect_ratio(image.size, (100, 100))
+
+def test_new_size_by_croping_ratio():
+    print(new_size_by_croping_ratio(image.size, (100, 100)))
+
+def test_new_size_by_croping():
+    new_size_by_croping(image.size,(100,100))
 
 def test_get_pygame_image():
     get_pygame_image(name, (100,100))
